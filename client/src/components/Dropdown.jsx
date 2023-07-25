30bbed52e7d28a58a50d5abe75ecd31cae28c665
sam tessema <99148321+samtessema9@users.noreply.github.com>
2023-07-24 19:19:20 -0700

    search functionality working - styling not complete for dropdown

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PrimaryContext } from '../context/PrimaryContext';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -10,8 +13,24 @@ const style = {
 };
 
 const DropDownItems = ( {array} ) => {
+  const navigate = useNavigate()
+  const {setCurrentVenue} = useContext(PrimaryContext)
+
+  const handleClick = (e) => {
+    const currentVenue = array.filter(venue => {
+      return venue.name == e.target.textContent
+    })
+    setCurrentVenue(currentVenue[0])
+    navigate('/details')
+  }
+
   return (
-    <List sx={style} component="nav" aria-label="mailbox folders">
+    <List 
+      sx={style} 
+      component="nav" 
+      aria-label="mailbox folders"
+      onClick={handleClick}
+    >
       {array.map(venue => {
         return (
             <>
