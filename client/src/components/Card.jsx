35d37b5c5d0b0872393c44aac13329d96c52ee6f
sam <samtessema09@gmail.com>
2023-09-11 23:35:25 -0700

    added favoriting but users need to be back filled to include a favoriteVenues property for this to work

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,59 @@
 import {useState, useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PrimaryContext } from '../context/PrimaryContext';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import StarRating from './Rating';
-import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardMedia, CardContent, Typography, Button, IconButton } from '@mui/material';
+import TurnedInSharpIcon from '@mui/icons-material/TurnedInSharp';
+import axios from 'axios';
 
 const VenueCard = ({venue}) => {
-  const {setCurrentVenue} = useContext(PrimaryContext);
+  const {setCurrentVenue, isLoggedIn, loggedInUser} = useContext(PrimaryContext);
   const navigate = useNavigate()
+  const queryClient = useQueryClient();
+
+
+  const updateUserFavorites = async ({ userId, venueId, isFavorite }) => {
+    const updatedUser = {
+      ...loggedInUser,
+      favoriteVenues: isFavorite
+        ? [...loggedInUser.favoriteVenues, venueId] // Add venueId to favorites
+        : loggedInUser.favoriteVenues.filter((id) => id !== venueId), // Remove venueId from favorites
+    };
+
+    const response = await axios.patch({
+        url: `http://localhost:3001/users/${userId}`,
+        method: 'PATCH',
+        data: {
+          favoriteVenues: updatedUser.favoriteVenues,
+        }
+    });
+
+    return response.data; 
+  };
+
+  const mutation = useMutation(updateUserFavorites, {
+    onMutate: ({ venueId, isFavorite }) => {
+      const updatedUser = {
+        ...loggedInUser,
+        favoriteVenues: isFavorite
+          ? [...loggedInUser.favoriteVenues, venueId]
+          : loggedInUser.favoriteVenues.filter((id) => id !== venueId),
+      };
+      queryClient.setQueryData(['user', loggedInUser.id], updatedUser);
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(['user', loggedInUser.id]);
+    },
+  });
+
+  const handleToggleFavorite = (venueId) => {
+    if (loggedInUser) {
+      const isFavorite = loggedInUser.favoriteVenues.includes(venueId);
+      mutation.mutate({ userId: loggedInUser.id, venueId, isFavorite });
+    }
+  };
 
   const handleClick = (e) => {
       setCurrentVenue(venue);
@@ -30,6 +77,14 @@ const VenueCard = ({venue}) => {
           <StarRating rating={venue.rating} />
           <p>{venue.hours.start} - {venue.hours.end}</p>
           <Button variant="text" color="primary" onClick={handleClick}>Details</Button>
+          {isLoggedIn && (
+            <IconButton
+              color={loggedInUser.favoriteVenues.includes(venue._id) ? 'secondary' : 'default'}
+              onClick={ () => handleToggleFavorite(venue._id)}
+            >
+              <TurnedInSharpIcon />
+            </IconButton>
+          )}
         </Typography>
       </CardContent>
     </Card>
